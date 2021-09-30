1f3df2a455fe63db55efb9325a3b3c9db278feb9
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-09-30 19:50:53 +0300

    added components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MenuAppBar from './components/AppBar';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     <MenuAppBar />
+     {/* <Signin /> */}
+     {/* <Signup /> */}
+     {/* <Dashboard /> */}
+    </div>
+  );
+}
+
+export default App;
