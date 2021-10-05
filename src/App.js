5afe4ab11aa88d51f5c1cb889121644f889206f7
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-10-06 00:54:47 +0300

    added dashboard, profile, newblog and login/logout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import MenuAppBar from './components/AppBar';
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import Dashboard from './components/Dashboard';
-
+import "./App.css";
+import AuthContextProvider from "./context/AuthContextProvider";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+import { BlogContextProvider } from "./context/BlogContextProvider";
 function App() {
   return (
     <div className="App">
-     <MenuAppBar />
-     {/* <Signin /> */}
-     {/* <Signup /> */}
-     {/* <Dashboard /> */}
+      <AuthContextProvider>
+        <BlogContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </BlogContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
